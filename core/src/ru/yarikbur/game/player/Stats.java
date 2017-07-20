d31393ae17ad6d9496d4291738f2cdb2fcd72d47
YarikBur <yarikbur@Mail.ru>
2017-07-21 01:13:09 +0300

    System of statistics

--- core/src/ru/yarikbur/game/player/Stats.java
+++ core/src/ru/yarikbur/game/player/Stats.java
@@ -1,35 +1,84 @@
 package ru.yarikbur.game.player;
 
 public class Stats {
-	private static String Name = "NULL"; // Имя
-	private static float Mana = 100f;  // Мана
-	private static float Health = 200f; // Жизни
-	private static float Force = 10f; // Сила
-	private static float Stamina = 9f; // Выносливость
-	private static float Adroitness = 11f; // Ловкость
-	private static float Intelligence = 8f; // Интеллект
-	private static float Experience = 6f; // Опыт
-	private static float Luck = 9f; // Удача
+	private static long startTime;
 	
+	private static String Name = "NULL"; // Имя
+	private static int ManaMax = 100;  // Мана Максимум
+	private static int HealthMax = 200; // Жизни Максимум
+	private static int Mana = ManaMax;  // Мана
+	private static int Health = HealthMax; // Жизни
+	private static int Force = 10; // Сила
+	private static int Stamina = 9; // Выносливость
+	private static int Adroitness = 11; // Ловкость
+	private static int Intelligence = 8; // Интеллект
+	private static int Experience = 0; // Опыт
+	private static int Luck = 9; // Удача
+	private static int Wisdom = 6; // Мудрость
+	private static int regenerationManaAdd = 0;
+	private static int regenerationHealthAdd = 0;
+	private static double regenerationMana=0;
+	private static double regenerationHealth=0;
+	
+	private static int Int = Intelligence;
+	private static int St = Stamina;
+	private static float time = 0;
 	
 	public String getName(){ return Name; }
-	public float getMana(){ return Mana; }
-	public float getHealth(){ return Health; }
-	public float getForce(){ return Force; }
-	public float getStamina(){ return Stamina; }
-	public float getAdroitness(){ return Adroitness; }
-	public float getIntelligence(){ return Intelligence; }
-	public float getExperience(){ return Experience; }
-	public float getLuck(){ return Luck; }
+	public int getMana(){ return Mana; }
+	public int getHealth(){ return Health; }
+	public int getManaMax(){ return ManaMax; }
+	public int getHealthMax(){ return HealthMax; }
+	public int getForce(){ return Force; }
+	public int getStamina(){ return Stamina; }
+	public int getAdroitness(){ return Adroitness; }
+	public int getIntelligence(){ return Intelligence; }
+	public int getExperience(){ return Experience; }
+	public int getLuck(){ return Luck; }
+	public int getWisdom(){ return Wisdom; }
+	public double getRegenerationMana(){ return regenerationMana; }
+	public double getRegenerationHealth(){ return regenerationHealth; }
 	
 	
 	public void setName(String Name){ Stats.Name = Name; }
-	public void setMana(float Mana){ Stats.Mana = Mana; }
-	public void setHealth(float Health){ Stats.Health = Health; }
-	public void setForce(float Force){ Stats.Force = Force; }
-	public void setStamina(float Stamina){ Stats.Stamina = Stamina; }
-	public void setAdroitness(float Adroitness){ Stats.Adroitness = Adroitness; }
-	public void setIntelligence(float Intelligence){ Stats.Intelligence = Intelligence; }
-	public void setExperience(float Experience){ Stats.Experience = Experience; }
-	public void setLuck(float Luck){ Stats.Luck = Luck; }
+	public void setMana(int Mana){ Stats.Mana = Mana; }
+	public void setHealth(int Health){ Stats.Health = Health; }
+	public void setManaMax(int Mana){ Stats.ManaMax = Mana; }
+	public void setHealthMax(int Health){ Stats.HealthMax = Health; }
+	public void setForce(int Force){ Stats.Force = Force; }
+	public void setStamina(int Stamina){ Stats.Stamina = Stamina; }
+	public void setAdroitness(int Adroitness){ Stats.Adroitness = Adroitness; }
+	public void setIntelligence(int Intelligence){ Stats.Intelligence = Intelligence; }
+	public void setExperience(int Experience){ Stats.Experience = Experience; }
+	public void setLuck(int Luck){ Stats.Luck = Luck; }
+	public void setWisdom(int Wisdom){ Stats.Wisdom = Wisdom; }
+	public void setStartTime(long startTime){ Stats.startTime = startTime; }
+	
+	public void regenerationStats(){
+		regenerationMana = (Wisdom/100*5 + ManaMax/100*2 + regenerationManaAdd);
+		regenerationHealth = (Stamina/100*5 + HealthMax/100*1 + regenerationHealthAdd);
+		if(time!=(System.currentTimeMillis() - startTime) / 1000){
+			time++;
+			
+			if(Mana!=ManaMax) {
+				Mana += regenerationMana;
+				if(Mana>ManaMax) Mana=ManaMax;
+			}
+			if(Health!=HealthMax) {
+				Health += regenerationHealth;
+				if(Health>HealthMax) Health=HealthMax;
+			}
+		}
+	}
+	
+	public void updateStats(){
+		if(Intelligence!=Int){
+			ManaMax += ManaMax/100*4;
+			Int++;
+		}
+		if(Stamina!=St){
+			HealthMax += HealthMax/100*3;
+			St++;
+		}
+	}
 }
--- core/src/ru/yarikbur/menu/Menu.java
+++ core/src/ru/yarikbur/menu/Menu.java
@@ -17,20 +17,38 @@ public class Menu implements Screen {
 	private All input = new All();
 	
 	private String location = "menu";
+	private ru.yarikbur.game.player.Stats stats;
 	
 	@SuppressWarnings("static-access")
 	@Override
 	public void show() {
 		batch = Main.getBatch();
 		font = new BitmapFont();
+		stats = new ru.yarikbur.game.player.Stats();
 		
 		Gdx.input.setInputProcessor(input.getInputMultiplexer());
+		stats.setStartTime(System.currentTimeMillis());
 	}
 	
 	@SuppressWarnings("static-access")
 	private void mainText(){
 		if(input.getDebug()) font.draw(batch, "FPS: "+Gdx.graphics.getFramesPerSecond(), 0, Gdx.graphics.getHeight()-1);
 		if(input.getDebug() && input.getKeycode() != 0) font.draw(batch, "Key: "+input.getKeycode(), 0, Gdx.graphics.getHeight()-15);
+	}
+	
+	private void statsPlayerDraw(){
+		font.draw(batch, "Name: " + stats.getName(), 0, Gdx.graphics.getHeight()-30);
+		if(stats.getHealth()==stats.getHealthMax()) font.draw(batch, "Health: " + stats.getHealth(), 0, Gdx.graphics.getHeight()-50);
+		else font.draw(batch, "Health: " + stats.getHealth() + "/" + stats.getHealthMax() + "   Regeneration "+ (stats.getRegenerationHealth()*60) +" per minute", 0, Gdx.graphics.getHeight()-50);
+		if(stats.getMana()==stats.getManaMax()) font.draw(batch, "Mana: " + stats.getMana(), 0, Gdx.graphics.getHeight()-65);
+		else font.draw(batch, "Mana: " + stats.getMana() + "/" + stats.getManaMax() + "   Regeneration "+ (stats.getRegenerationMana()*60) +" per minute", 0, Gdx.graphics.getHeight()-65);
+		font.draw(batch, "Force: " + stats.getForce(), 0, Gdx.graphics.getHeight()-80);
+		font.draw(batch, "Stamina: " + stats.getStamina(), 0, Gdx.graphics.getHeight()-95);
+		font.draw(batch, "Adroitness: " + stats.getAdroitness(), 0, Gdx.graphics.getHeight()-110);
+		font.draw(batch, "Intelligence: " + stats.getIntelligence(), 0, Gdx.graphics.getHeight()-125);
+		font.draw(batch, "Luck: " + stats.getLuck(), 0, Gdx.graphics.getHeight()-140);
+		font.draw(batch, "Wisdom: " + stats.getWisdom(), 0, Gdx.graphics.getHeight()-155);
+		font.draw(batch, "Experience: " + stats.getExperience(), 0, Gdx.graphics.getHeight()-175);
 	}
 	
 	@SuppressWarnings("static-access")
@@ -42,15 +60,22 @@ public class Menu implements Screen {
 	float r=0,g=0,b=0;
 	boolean upR=true, upG=true, upB=true;
 	
+	@SuppressWarnings("static-access")
 	@Override
 	public void render(float delta) {
+		stats.updateStats();
+		stats.regenerationStats();
 		Gdx.gl.glClearColor(r, g, b, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
 		batch.begin();
 		if(location.equals("menu")) rendMenu();
+		if(input.getKeycode()==37) stats.setIntelligence(stats.getIntelligence()+1); // I
+		if(input.getKeycode()==47) stats.setStamina(stats.getStamina()+1); // S
+		if(input.getKeycode()==51) stats.setWisdom(stats.getWisdom()+1); // W
 		
 		mainText();
+		statsPlayerDraw();
 		batch.end();
 		
 		if(upR){
